08c9346b0f2089de560b5b7c413b2182f2df85eb
Shreedharpatil183 <shreedharpatil183@gmail.com>
2026-04-18 14:25:26 +0530

    initial fix

--- creditcardapplication/src/main/java/dao/UserRepository.java
+++ creditcardapplication/src/main/java/dao/UserRepository.java
@@ -1,6 +1,8 @@
 package dao;
 
+import entity.UserInfo;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepositoriesAutoConfiguration<U> {
+public interface UserRepository extends JpaRepository<UserInfo, String> {
 }
